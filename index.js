6d5c2aa3c93b5bd8c9320dd111ad8abcbd0a7ad9
Karina Pliego <karinapliegovergara@gmail.com>
2023-04-26 10:28:11 -0500

    completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+//multiply takes two numbers and gives us product
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+//random gives us integer greater than 0
+let random = Math.round(Math.random() * 10);
+
+//mod of two numbers
+let num3 = 24;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+//max finds highest number in a set
+let max = Math.max(3, 7, 12, 4, 13, 20);
